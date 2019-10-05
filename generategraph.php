01f01ec254b0a9a798a1756b2198370a8129804c
Harsh Thakkar <harshthakkar456@gmail.com>
2019-10-05 10:27:34 -0400

    Update generategraph.php

--- generategraph.php
+++ generategraph.php
@@ -6,8 +6,6 @@ include('accesscontrol.php');
 
 $data_points=array();
 
-$query = "SELECT x, y FROM Graph_Points WHERE sample_id=1"
-$result=mysqli_query($mysqli, $query);
 
 while($row=mysqli_fetch_array($result)){
 $point = array("x"=> $row['x'], "y"=> $row['y']);
