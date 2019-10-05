340c8f2bfda69a973d34a687def14fe1e261ca92
Harsh Thakkar <harshthakkar456@gmail.com>
2019-10-05 10:18:40 -0400

    Update saveoperator.php

--- saveoperator.php
+++ saveoperator.php
@@ -19,17 +19,13 @@
 	$oname = filter_var($_POST["operator_name"], FILTER_SANITIZE_STRING);
 
 	if(empty($oid)){
-		$statement = $mysqli->prepare("INSERT INTO Operators (name) VALUES(?)");
-		$statement->bind_param('s', $oname);
-	
+		
 		if($statement->execute()){
 			print "Operator has been created";		
 		} else {
 			print $mysqli->error;
 		}
 	} else {
-		$statement = $mysqli->prepare("UPDATE Operators SET name = ? WHERE Operators.id = ?");
-		$statement->bind_param('si', $oname, $oid);
 		if($statement->execute()){
 			print "Operator has been updated";		
 		} else {
