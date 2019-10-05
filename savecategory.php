b259eb281a6104f1dbd009f2db971cad083b626f
Harsh Thakkar <harshthakkar456@gmail.com>
2019-10-05 10:21:05 -0400

    Update savecategory.php

--- savecategory.php
+++ savecategory.php
@@ -19,17 +19,13 @@
 	$cname = filter_var($_POST["category_name"], FILTER_SANITIZE_STRING);
 
 	if(empty($cid)){
-		$statement = $mysqli->prepare("INSERT INTO Categories (name) VALUES(?)");
-		$statement->bind_param('s', $cname);
-	
+		
 		if($statement->execute()){
 			print "Category has been created";		
 		} else {
 			print $mysqli->error;
 		}
 	} else {
-		$statement = $mysqli->prepare("UPDATE Categories SET name = ? WHERE Categories.id = ?");
-		$statement->bind_param('si', $cname, $cid);
 		if($statement->execute()){
 			print "Category has been updated";		
 		} else {
